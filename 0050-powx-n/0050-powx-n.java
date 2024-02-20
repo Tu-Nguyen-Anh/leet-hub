77e424048525b5b7a274656dfaea49b88f6d78fe
Nguyễn Anh Tú <157774605+TuNA-IT@users.noreply.github.com>
2024-02-20 09:46:07 +0700

    Time: 0 ms (100.00%), Space: 42.6 MB (22.41%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static double myPow(double x, int n) {
+        if (x == 0) {
+            return 0; // Trường hợp x = 0, kết quả sẽ luôn là 0.
+        }
+        if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            if (n == Integer.MIN_VALUE) {
+                return 1 / (x * myPow(x, -(n + 1)));
+            } else {
+                return 1 / myPow(x, -n);
+            }
+        } else if (n % 2 == 0) {
+            double halfPow = myPow(x, n / 2);
+            return halfPow * halfPow;
+        } else {
+            return x * myPow(x, n - 1);
+        }
+    }
+}
